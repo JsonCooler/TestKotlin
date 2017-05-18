ba3bffaf84d768e36712f54c44e1717cb1f970f3
JsonCooler <401532916@qq.com>
2017-05-18 16:37:26 +0800

    新增小肉

---
+++ app/src/main/java/zfc/com/testkotlin/小肉.java
@@ -0,0 +1,17 @@
+package zfc.com.testkotlin;
+
+/**
+ * Created by lenovo on 2017/5/18.
+ */
+
+public class 小肉 {
+    private String 肉;
+
+    public String get肉() {
+        return 肉;
+    }
+
+    public void set肉(String 肉) {
+        this.肉 = 肉;
+    }
+}
